7c507422d1b727583705d74af0bdaad5d488a03d
Viktor (Makflay) <chief.makflay1@gmail.com>
2026-02-14 22:14:15 +0300

    add user model

---
+++ backend/src/models/UserShema.ts
@@ -0,0 +1,51 @@
+import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcrypt";
+import { IUser } from "./IUser";
+
+const UserSchema: Schema = new Schema<IUser>(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+UserSchema.pre<IUser>("save", async function () {
+  if (!this.isModified("password")) return;
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+  } catch (error) {
+    throw error;
+  }
+});
+
+UserSchema.methods.comparePassword = async function (
+  candidatePassword: string,
+): Promise<boolean> {
+  return bcrypt.compare(candidatePassword, this.password);
+};
+
+const User = mongoose.model<IUser>("User", UserSchema);
+export default User;
