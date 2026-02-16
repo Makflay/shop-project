3cd3ea34613fd537906d727ab9c45d33a5daefa1
Viktor (Makflay) <chief.makflay1@gmail.com>
2026-02-16 12:25:57 +0300

    Add auth routes

---
+++ backend/src/controllers/auth.controller.ts
@@ -0,0 +1,31 @@
+import { Request, Response } from "express";
+import * as authService from "../services/auth.service";
+
+export const register = async (req: Request, res: Response) => {
+  try {
+    const user = await authService.registerUser(req.body);
+    res.status(201).json({
+      id: user._id,
+      name: user.name,
+      email: user.email,
+      role: user.role,
+    });
+  } catch (error: any) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+export const login = async (req: Request, res: Response) => {
+  try {
+    const { user, token } = await authService.loginUser(req.body);
+    res.json({
+      id: user._id,
+      name: user.name,
+      email: user.email,
+      role: user.role,
+      token,
+    });
+  } catch (error: any) {
+    res.status(400).json({ message: error.message });
+  }
+};
