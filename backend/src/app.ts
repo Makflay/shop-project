098398f68b149c77b746fcab322be7fc5bca1a11
Viktor (Makflay) <chief.makflay1@gmail.com>
2026-02-16 18:39:17 +0300

    Fix app.ts logic

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,14 @@
 import express from "express";
+import authRoutes from "./routes/auth.routes";
+import productRoutes from "./routes/product.routes";
 const app = express();
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+
+app.use("/api/auth", authRoutes);
+app.use("/api/products", productRoutes);
 
 export default app;
