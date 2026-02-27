c682173a283e9de711717e16c9c79c4b593febf1
Viktor (Makflay) <chief.makflay1@gmail.com>
2026-02-27 19:38:02 +0300

    chore: update configs and setup files

---
+++ backend/src/config/seed.admin.ts
@@ -0,0 +1,32 @@
+import User from "../models/UserShema";
+
+export const seedAdmin = async () => {
+  try {
+    const adminName = process.env.ADMIN_NAME;
+    const adminEmail = process.env.ADMIN_EMAIL;
+    const adminPassword = process.env.ADMIN_PASSWORD;
+
+    if (!adminEmail || !adminPassword || !adminName) {
+      console.error("Admin credentials are not set in .env");
+      return;
+    }
+
+    const existingAdmin = await User.findOne({ email: adminEmail });
+    if (existingAdmin) {
+      console.log("Admin already exists");
+      return;
+    }
+
+    const newAdmin = new User({
+      name: adminName,
+      email: adminEmail,
+      password: adminPassword,
+      role: "admin",
+    });
+
+    await newAdmin.save();
+    console.log("Admin created successfully");
+  } catch (error) {
+    console.error("Error seeding admin:", error);
+  }
+};
