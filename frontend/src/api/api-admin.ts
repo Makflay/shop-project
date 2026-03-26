47ebec37dae971bb8cc15d3d7f4b8a24a108d4c0
Viktor (Makflay) <chief.makflay1@gmail.com>
2026-03-26 21:06:01 +0300

    refactor: remove deprecated admin api

--- frontend/src/api/api-admin.ts
+++
@@ -1,7 +0,0 @@
-import apiClient from "./api-client";
-import type { IAdminStats } from "../types/admin-stats";
-
-export const getAdminStats = async (): Promise<IAdminStats> => {
-  const res = await apiClient("/admin/stats");
-  return res.data;
-};
